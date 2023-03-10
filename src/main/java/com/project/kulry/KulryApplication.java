66e82ca112b884d719e7aa2af3e832560579f1e9
Min <74136649+DANGHYEON@users.noreply.github.com>
2023-03-10 15:16:20 +0900

    초기설정 version 1

---
+++ src/main/java/com/project/kulry/KulryApplication.java
@@ -0,0 +1,13 @@
+package com.project.kulry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KulryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KulryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/kulry/KulryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kulry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KulryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
